eb37ee9d627cd974852f443b202383f21b59ce90
Alex Tranchenko <tranchenkoa@gmail.com>
2018-08-07 22:03:46 +0300

    added mobile view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'GoF Design patterns';
+  title = 'GoF design patterns';
 }
