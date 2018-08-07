475e7597c662f87325606b6eeec25a9f10c2fc37
Alex Tranchenko <tranchenkoa@gmail.com>
2018-08-07 11:10:01 +0300

    switch to hash routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import {ROUTES} from './routes';
 
 
 @NgModule({
-  imports: [RouterModule.forRoot(ROUTES)],
+  imports: [RouterModule.forRoot(ROUTES, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
