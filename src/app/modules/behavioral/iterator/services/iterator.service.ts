9d1011f000107c705cc81be8ad3cea3176bc74f5
Alex Tranchenko <tranchenkoa@gmail.com>
2018-08-15 22:14:38 +0300

    update behavioral/iterator pattern example

--- src/app/modules/behavioral/iterator/services/iterator.service.ts
+++ src/app/modules/behavioral/iterator/services/iterator.service.ts
@@ -11,20 +11,29 @@ export class IteratorService {
 
   iterator() {
     const map = new Map();
-    // console.log('map.size', map.size);
-    // console.log('map.set(\'first\', 1)', map.set('first', 1));
-    // console.log('map.set(\'second\', 1)', map.set('second', 1));
-    // console.log('map.size', map.size); // 2
-    // console.log('map.set(\'third\', 1)', map.set('third', 1));
-    // console.log('map.set(\'third\', 1)', map.set('third', 1)); // Error
-    // console.log('map.size', map.size); // 3
-    // console.log('map.set(\'forth\', 1)', map.set('forth', 1));
-    // console.log('map.size', map.size); // 4
-    // console.log('map.get(\'first\')', map.get('first'));
-    // console.log('map.get(\'none\')', map.get('none'));
-    // map.delete('none');
-    // console.log('map.size', map.size);
-    // map.delete('first');
-    // console.log('map.size', map.size);
+    console.log('map.size', map.size);
+    console.log('map.set(\'first\', 1)', map.set('first', 1));
+    console.log('map.set(\'second\', 1)', map.set('second', 2));
+    console.log('map.size', map.size); // 2
+    console.log('map.set(\'third\', 1)', map.set('third', 3));
+    console.log('map.set(\'third\', 1)', map.set('third', 55)); // Error
+    console.log('map.size', map.size); // 3
+    console.log('map.set(\'forth\', 1)', map.set('forth', 4));
+    console.log('map.size', map.size); // 4
+    console.log('map.get(\'first\')', map.get('first'));
+    console.log('map.get(\'none\')', map.get('none'));
+    map.delete('none');
+    map.delete('first');
+    console.log('map', map); // 2
+
+    const keyIterator = map.createKeyIterator();
+    for (let i = keyIterator.first(); !keyIterator.isDone(); i = keyIterator.next()) {
+      console.log(i);
+    }
+    const valIterator = map.createValIterator();
+    for (let i = valIterator.first(); !valIterator.isDone(); i = valIterator.next()) {
+      console.log(i);
+    }
+
   }
 }
--- src/app/modules/behavioral/iterator/services/iterator/iterator.ts
+++ src/app/modules/behavioral/iterator/services/iterator/iterator.ts
@@ -1,77 +1,36 @@
 abstract class Collection {
   public keyVault: Array<any> = [];
   public valueVault: Array<any> = [];
-  protected mapSize: number;
 
-  protected abstract getEnumerator(): Enumerator;
+  public abstract createKeyIterator(): KeyIterator;
 }
 
 export class Map extends Collection {
-  private iterator: Enumerator;
-
   constructor() {
     super();
-    this.iterator = this.getEnumerator();
-    this.mapSize = this.iterator.size();
+  }
+
+  createKeyIterator() {
+    return new KeyIterator(this);
+  }
+
+  createValIterator() {
+    return new ValIterator(this);
   }
 
   public get size(): number {
-    return this.mapSize;
-  }
-
-  public has(key): boolean {
-    return this.iterator.has(key);
-  }
-
-  public set(key: any, val: any): any {
-    const res = this.iterator.set(key, val);
-    this.mapSize = this.iterator.size();
-    return res;
-  }
-
-  public get(key: any): any {
-    return this.iterator.get(key);
-  }
-
-  public delete(key: any) {
-    this.iterator.delete(key);
-    this.mapSize = this.iterator.size();
-  }
-
-  protected getEnumerator(): Enumerator {
-    return new Iterator(this);
-  }
-}
-
-interface Enumerator {
-  size(): number;
-
-  has(key: any): boolean;
-
-  set(key: any, val: any): boolean | Error;
-
-  get(key: any): any;
-
-  delete(key: any): void;
-}
-
-export class Iterator implements Enumerator {
-  constructor(public iterable: Collection) {
-  }
-
-  size(): number {
-    return this.iterable.keyVault.length;
+    return this.keyVault.length;
   }
 
   has(key: any): boolean {
-    return this.iterable.keyVault.includes(key);
+    return this.keyVault.includes(key);
   }
 
   set(key: any, val: any): boolean | Error {
     let res: boolean | Error;
     if (!this.has(key)) {
-      this.iterable.keyVault.push(key);
-      this.iterable.valueVault.push(val);
+      this.keyVault.push(key);
+      this.valueVault.push(val);
       res = this.get(key) === val;
     } else {
       res = new Error('Key has been existing!');
@@ -81,19 +40,84 @@ export class Iterator implements Enumerator {
 
   get(key: any): any {
     const ind = this.getInd(key);
-    return ind !== -1 ? this.iterable.valueVault[ind] : false;
+    return ind !== -1 ? this.valueVault[ind] : false;
   }
 
   delete(key: any): boolean {
     const ind = this.getInd(key);
     if (ind !== -1) {
-      this.iterable.keyVault.splice(ind, 1);
-      this.iterable.valueVault.splice(ind, 1);
+      this.keyVault.splice(ind, 1);
+      this.valueVault.splice(ind, 1);
     }
     return this.has(key);
   }
 
   private getInd(key: any): number {
-    return this.iterable.keyVault.indexOf(key);
+    return this.keyVault.indexOf(key);
   }
+}
+
+interface Enumerator {
+  first(): any;
+
+  currentItem(): any;
+
+  next(): any;
+
+  isDone(): boolean;
+}
+
+abstract class Iterator implements Enumerator {
+  protected current: number;
+
+  constructor(public iterable: Map) {
+  }
+
+  protected abstract getItem(ind: number);
+
+  first(): any {
+    return this.getItem(0);
+  }
+
+  currentItem(): any {
+    return this.getItem(this.current);
+  }
+
+  next(): any {
+    return this.current++ < this.iterable.size - 1 ? this.getItem(this.current) : null;
+  }
+
+  isDone(): boolean {
+    if (this.current < this.iterable.size) {
+      return false;
+    } else {
+      this.current = 0;
+      return true;
+    }
+  }
+}
+
+export class KeyIterator extends Iterator {
+  protected current = 0;
+
+  constructor(public iterable: Map) {
+    super(iterable);
+  }
+
+  protected getItem(ind: number) {
+    return this.iterable.keyVault[ind];
+  }
+}
+
+export class ValIterator extends Iterator {
+  protected current = 0;
+
+  constructor(public iterable: Map) {
+    super(iterable);
+  }
+
+  protected getItem(ind: number) {
+    return this.iterable.valueVault[ind];
+  }
+
 }
