e9bd45cc93b413cfe67679c90e660b2eee98ca79
Alex Tranchenko <tranchenkoa@gmail.com>
2018-08-26 19:26:31 +0300

    updated components

--- src/app/modules/behavioral/iterator/services/iterator/iterator.ts
+++ src/app/modules/behavioral/iterator/services/iterator/iterator.ts
@@ -1,28 +1,16 @@
 export function iterator() {
   const map = new Map();
-  // console.log('map.size', map.size);
-  // console.log('map.set(\'first\', 1)', map.set('first', 1));
-  // console.log('map.set(\'second\', 1)', map.set('second', 2));
-  // console.log('map.size', map.size); // 2
-  // console.log('map.set(\'third\', 1)', map.set('third', 3));
-  // console.log('map.set(\'third\', 1)', map.set('third', 55)); // Error
-  // console.log('map.size', map.size); // 3
-  // console.log('map.set(\'forth\', 1)', map.set('forth', 4));
-  // console.log('map.size', map.size); // 4
-  // console.log('map.get(\'first\')', map.get('first'));
-  // console.log('map.get(\'none\')', map.get('none'));
-  // map.delete('none');
-  // map.delete('first');
-  // console.log('map', map); // 2
-
-  const keyIterator = map.createKeyIterator();
-  for (let i = keyIterator.first(); !keyIterator.isDone(); i = keyIterator.next()) {
-    console.log(i);
-  }
-  const valIterator = map.createValIterator();
-  for (let i = valIterator.first(); !valIterator.isDone(); i = valIterator.next()) {
-    console.log(i);
-  }
+  map.set('first', 1);
+  map.set('second', 2);
+  map.set('third', 3);
+  // const keyIterator = map.createKeyIterator();
+  // for (let i = keyIterator.first(); !keyIterator.isDone(); i = keyIterator.next()) {
+  //   console.log(i);
+  // }
+  // const valIterator = map.createValIterator();
+  // for (let i = valIterator.first(); !valIterator.isDone(); i = valIterator.next()) {
+  //   console.log(i);
+  // }
 }
 
 abstract class Collection {
@@ -30,6 +18,7 @@ abstract class Collection {
   public valueVault: Array<any> = [];
 
   public abstract createKeyIterator(): Iterator;
+
   public abstract createValIterator(): Iterator;
 }
 
