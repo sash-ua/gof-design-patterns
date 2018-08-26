df25d01274d5d4bcb9181b0832e379ef0a521132
Alex Tranchenko <tranchenkoa@gmail.com>
2018-08-26 21:35:24 +0300

    updated iterator pattern example

--- src/app/modules/behavioral/iterator/services/iterator/iterator.ts
+++ src/app/modules/behavioral/iterator/services/iterator/iterator.ts
@@ -1,80 +1,21 @@
+import {Map} from '../map';
+
 export function iterator() {
   const map = new Map();
   map.set('first', 1);
   map.set('second', 2);
   map.set('third', 3);
-  // const keyIterator = map.createKeyIterator();
-  // for (let i = keyIterator.first(); !keyIterator.isDone(); i = keyIterator.next()) {
-  //   console.log(i);
-  // }
-  // const valIterator = map.createValIterator();
-  // for (let i = valIterator.first(); !valIterator.isDone(); i = valIterator.next()) {
-  //   console.log(i);
-  // }
-}
-
-abstract class Collection {
-  public keyVault: Array<any> = [];
-  public valueVault: Array<any> = [];
-
-  public abstract createKeyIterator(): Iterator;
-
-  public abstract createValIterator(): Iterator;
-}
-
-export class Map extends Collection {
-  constructor() {
-    super();
+  const keyIterator = map.createKeyIterator();
+  for (let i = keyIterator.first(); !keyIterator.isDone(); i = keyIterator.next()) {
+    // console.log(i);
   }
-
-  private getInd(key: any): number {
-    return this.keyVault.indexOf(key);
-  }
-
-  public createKeyIterator() {
-    return new KeyIterator(this);
-  }
-
-  public createValIterator() {
-    return new ValIterator(this);
-  }
-
-  public get size(): number {
-    return this.keyVault.length;
-  }
-
-  public has(key: any): boolean {
-    return this.keyVault.includes(key);
-  }
-
-  public set(key: any, val: any): boolean | Error {
-    let res: boolean | Error;
-    if (!this.has(key)) {
-      this.keyVault.push(key);
-      this.valueVault.push(val);
-      res = this.get(key) === val;
-    } else {
-      res = new Error('Key has been existing!');
-    }
-    return res;
-  }
-
-  public get(key: any): any {
-    const ind = this.getInd(key);
-    return ind !== -1 ? this.valueVault[ind] : false;
-  }
-
-  public delete(key: any): boolean {
-    const ind = this.getInd(key);
-    if (ind !== -1) {
-      this.keyVault.splice(ind, 1);
-      this.valueVault.splice(ind, 1);
-    }
-    return this.has(key);
+  const valIterator = map.createValIterator();
+  for (let i = valIterator.first(); !valIterator.isDone(); i = valIterator.next()) {
+    // console.log(i);
   }
 }
 
-interface Enumerator {
+export interface Enumerator {
   first(): any;
 
   currentItem(): any;
@@ -84,7 +25,7 @@ interface Enumerator {
   isDone(): boolean;
 }
 
-abstract class Iterator implements Enumerator {
+export abstract class Iterator implements Enumerator {
   protected current: number;
 
   protected constructor(public iterable: Map) {
---
+++ src/app/modules/behavioral/iterator/services/map.ts
@@ -0,0 +1,62 @@
+import {Iterator, KeyIterator, ValIterator} from './iterator/iterator';
+
+abstract class Collection {
+  public keyVault: Array<any> = [];
+  public valueVault: Array<any> = [];
+
+  public abstract createKeyIterator(): Iterator;
+
+  public abstract createValIterator(): Iterator;
+}
+
+export class Map extends Collection {
+  constructor() {
+    super();
+  }
+
+  private getInd(key: any): number {
+    return this.keyVault.indexOf(key);
+  }
+
+  public createKeyIterator() {
+    return new KeyIterator(this);
+  }
+
+  public createValIterator() {
+    return new ValIterator(this);
+  }
+
+  public get size(): number {
+    return this.keyVault.length;
+  }
+
+  public has(key: any): boolean {
+    return this.keyVault.includes(key);
+  }
+
+  public set(key: any, val: any): boolean | Error {
+    let res: boolean | Error;
+    if (!this.has(key)) {
+      this.keyVault.push(key);
+      this.valueVault.push(val);
+      res = this.get(key) === val;
+    } else {
+      res = new Error('Key has been existing!');
+    }
+    return res;
+  }
+
+  public get(key: any): any {
+    const ind = this.getInd(key);
+    return ind !== -1 ? this.valueVault[ind] : false;
+  }
+
+  public delete(key: any): boolean {
+    const ind = this.getInd(key);
+    if (ind !== -1) {
+      this.keyVault.splice(ind, 1);
+      this.valueVault.splice(ind, 1);
+    }
+    return this.has(key);
+  }
+}
